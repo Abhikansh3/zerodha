64e2d438f77abbc19b020567ba7f5adbaa4666ef
Abhikansh Mittal <abhikansh33@gmail.com>
2026-01-01 13:43:36 +0530

    Intial Commit

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,14 @@
+const Navbar = () => {
+  return (
+    <div>
+      <div>
+        <img src="https://zerodha.com/static/images/logo.svg" alt="" />
+      </div>
+
+      <div>
+        signup
+      </div>
+    </div>
+  )
+}
+export default Navbar
